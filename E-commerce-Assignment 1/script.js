c43f4efb3455f71edf83a1c320634f03f279df3d
ZainWare <68492335+ZainWare@users.noreply.github.com>
2024-03-05 01:06:23 +0200

    Delete E-commerce-Assignment 1 directory

--- E-commerce-Assignment 1/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var navigation = document.querySelector('.navigation');
-    navigation.classList.toggle('active');
-}
-
-function search() {
-    var searchInput = document.getElementById('search-input').value;
-    // Perform search functionality
-}
