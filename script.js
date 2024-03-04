678a9ea1679f08f58888ad7692386255ca0942d8
ZainWare <68492335+ZainWare@users.noreply.github.com>
2024-03-05 00:56:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+}
+
+function search() {
+    var searchInput = document.getElementById('search-input').value;
+    // Perform search functionality
+}
